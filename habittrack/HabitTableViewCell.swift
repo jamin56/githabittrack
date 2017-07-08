618c1c9c80492daaf0116e581441108f1237ee94
bgibson <bengibson@Bens-MacBook-Pro.local>
2017-07-08 22:28:25 +0900

    Created Table View and Cell

---
+++ habittrack/HabitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HabitTableViewCell.swift
+//  habittrack
+//
+//  Created by Ben Gibson on 7/8/17.
+//  Copyright © 2017 Wabi Sabi. All rights reserved.
+//
+
+import UIKit
+
+class HabitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
